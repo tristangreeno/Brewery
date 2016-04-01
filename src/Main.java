ed9353b942a9697c47d24a05130977d651b1212e
Tristan Greeno <greeno.tristan@gmail.com>
2016-04-01 14:45:38 -0500

    Updated main javadoc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 /**
- *
+ *This program is designed to store brew names, prices and sales into a hash map to reference later.
+ * The program will create the hash maps first using brewSales and brewNetProfits, and then display them.
+ * The sales and profits will be ordered from greatest to least in the display.
  */
 public class Main {
 
